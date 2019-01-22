442b4b263f03a2f7abac104b849774d8e9a7fa18
Dmytro Katyukha <cto@crnd.pro>
2019-01-22 13:25:42 +0200

    Basic implementation of crnd_web_button_box_full_width

---
+++ crnd_web_button_box_full_width/README.rst
@@ -0,0 +1,4 @@
+CRnD Button Box Full Width
+==========================
+
+When this module is installed, button_box will be placed at the top of the form
---
+++ crnd_web_button_box_full_width/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "CRND Web Button Box Fullwidth",
+    "version": "11.0.0.0.1",
+    "author": "Center of Research & Development",
+    "website": "https://crnd.pro",
+    "license": "LGPL-3",
+    'category': 'Technical Settings',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+}
