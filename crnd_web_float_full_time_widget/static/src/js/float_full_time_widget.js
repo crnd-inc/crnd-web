bb933f6c5d28a399183b6a5e9f7c1d9f46bbb41a
Ihor <bezoblachnyj@gmail.com>
2019-02-25 15:21:39 +0200

    Added widget float_full_time.

--- crnd_web_float_full_time_widget/static/src/js/float_full_time_widget.js
+++ crnd_web_float_full_time_widget/static/src/js/float_full_time_widget.js
@@ -1,24 +1,27 @@
-odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) {
+odoo.define('crnd_web_float_full_time_widget.FullFloatTime', function (require) {
 "use strict";
 
     var registry = require('web.field_registry');
     var basic_fields = require('web.basic_fields');
     var field_utils = require('web.field_utils');
+    var core = require('web.core');
 
-    var FloatFullTime = basic_fields.FieldFloat.extend({
+    var _t = core._t;
+
+    var FloatTimeDuration = basic_fields.FieldFloat.extend({
 
         init: function () {
             this._super.apply(this, arguments);
+            this.round_off = this.nodeOptions.round_off;
+            this.time_only = this.nodeOptions.time_only;
         },
 
-        formatFullFloatTime: function(value){
-            var round_off = this.nodeOptions.round_off;
-            var time_only = this.nodeOptions.time_only;
+        formatFloatTimeDuration: function(value){
             var pattern = '%02d:%02d:%02d';
-            if (time_only !== true) {
-                pattern = '%01dd:' + pattern;
+            if (this.time_only !== true) {
+                pattern = '%01d' + _t('d') + ' ' + pattern;
             }
-            if (round_off !== true) {
+            if (this.round_off !== true) {
                 pattern += ',%03d';
             }
             var in_value = value;
@@ -26,14 +29,14 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
                 in_value = Math.abs(value);
                 pattern = '-' + pattern;
             }
-            var time_values = this._get_time_value_for_pattern(round_off, time_only, in_value);
-            if (round_off === true && time_only === true) {
+            var time_values = this._get_time_value_for_pattern(in_value);
+            if (this.round_off === true && this.time_only === true) {
                 return _.str.sprintf(
                     pattern, time_values.hours, time_values.minutes, time_values.seconds);
-            } else if (round_off === true && time_only !== true) {
+            } else if (this.round_off === true && this.time_only !== true) {
                 return _.str.sprintf(
                     pattern, time_values.days, time_values.hours, time_values.minutes, time_values.seconds);
-            } else if (round_off !== true && time_only === true) {
+            } else if (this.round_off !== true && this.time_only === true) {
                 return _.str.sprintf(
                     pattern, time_values.hours, time_values.minutes, time_values.seconds, time_values.milliseconds);
             }
@@ -41,9 +44,7 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
                 pattern, time_values.days, time_values.hours, time_values.minutes, time_values.seconds, time_values.milliseconds);
         },
 
-        parseFullFloatTime: function(value){
-            var round_off = this.nodeOptions.round_off;
-            var time_only = this.nodeOptions.time_only;
+        parseFloatTimeDuration: function(value){
             var parse_integer = field_utils.parse.integer;
             var factor = 1;
             var in_value = value;
@@ -51,7 +52,7 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
                 in_value = value.slice(1);
                 factor = -1;
             }
-            var float_time_pair = in_value.split(":");
+            var float_time_pair = in_value.split(/,| |:/);
             if (float_time_pair.length < 3){
                 return factor * parseFloat(value);
             }
@@ -60,34 +61,31 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
             var minutes = 0;
             var seconds = 0;
             var milliseconds = 0;
-            var sec_millisec = '';
-            if (round_off === true && time_only === true) {
+            if (this.round_off === true && this.time_only === true) {
                 hours = parse_integer(float_time_pair[0]) * 3600;
                 minutes = parse_integer(float_time_pair[1]) * 60;
                 seconds = parse_integer(float_time_pair[2]);
-            } else if (round_off === true && time_only !== true) {
+            } else if (this.round_off === true && this.time_only !== true) {
                 days = parse_integer(float_time_pair[0].slice(0, -1)) * 86400;
                 hours = parse_integer(float_time_pair[1]) * 3600;
                 minutes = parse_integer(float_time_pair[2]) * 60;
                 seconds = parse_integer(float_time_pair[3]);
-            } else if (round_off !== true && time_only === true) {
-                sec_millisec = float_time_pair[2].split(",");
+            } else if (this.round_off !== true && this.time_only === true) {
                 hours = parse_integer(float_time_pair[0]) * 3600;
                 minutes = parse_integer(float_time_pair[1]) * 60;
-                seconds = parse_integer(sec_millisec[0]);
-                milliseconds = parse_integer(sec_millisec[1]) / 1000;
+                seconds = parse_integer(float_time_pair[2]);
+                milliseconds = parse_integer(float_time_pair[3]) / 1000;
             } else {
-                sec_millisec = float_time_pair[3].split(",");
                 days = parse_integer(float_time_pair[0].slice(0, -1)) * 86400;
                 hours = parse_integer(float_time_pair[1]) * 3600;
                 minutes = parse_integer(float_time_pair[2]) * 60;
-                seconds = parse_integer(sec_millisec[0]);
-                milliseconds = parse_integer(sec_millisec[1]) / 1000;
+                seconds = parse_integer(float_time_pair[3]);
+                milliseconds = parse_integer(float_time_pair[4]) / 1000;
             }
             return factor * (days + hours + minutes + seconds + milliseconds);
         },
 
-        _get_time_value_for_pattern: function (round_off, time_only, value) {
+        _get_time_value_for_pattern: function (value) {
             var total_sec = Math.floor(value);
             var milisec = Math.round(value % 1 * 1000);
             if (milisec === 1000){
@@ -99,7 +97,7 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
             var hours_in_sec = hours * 3600;
             var minutes = Math.floor((total_sec - hours_in_sec) / 60);
             var seconds = Math.floor(total_sec%60);
-            if (time_only !== true) {
+            if (this.time_only !== true) {
                 days = Math.floor(hours / 24);
                 hours -= days * 24;
             }
@@ -113,16 +111,43 @@ odoo.define('crnd_web_float_full_time_widget.FloatFullTime', function (require) 
         },
 
         _formatValue: function (value) {
-            return this.formatFullFloatTime(value);
+            return this.formatFloatTimeDuration(value);
         },
 
         _parseValue: function (value) {
-            return this.parseFullFloatTime(value);
+            return this.parseFloatTimeDuration(value);
         },
 
     });
 
+    var FloatFullTime = FloatTimeDuration.extend({
+
+        init: function () {
+            this._super.apply(this, arguments);
+            this.round_off = this.nodeOptions.round_off;
+            this.time_only = true;
+        },
+
+        isValid: function () {
+            this._super.apply(this, arguments);
+            if (Math.abs(this.value) > 86399.999) {
+                this.do_warn(
+                    _t('The value of the field: ') +
+                    this.field.string +
+                    _t(', can not be more than 23:59:59,999 sec! Please check it!')
+                );
+
+                this._isValid = false;
+            }
+            return this._isValid;
+        },
+    });
+
+    registry.add('float_time_duration', FloatTimeDuration);
     registry.add('float_full_time', FloatFullTime);
 
-    return FloatFullTime;
+    return {
+        FloatTimeDuration: FloatTimeDuration,
+        FloatFullTime: FloatFullTime
+    };
 });
