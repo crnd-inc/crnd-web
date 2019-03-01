c3e24b84b1e2ebcdd45e8c2bf2d8858d2a470793
Ihor <bezoblachnyj@gmail.com>
2019-03-01 11:18:47 +0200

    Small fix by pylint.

--- crnd_web_float_full_time_widget/static/tests/float_full_time_widget_tests.js
+++ crnd_web_float_full_time_widget/static/tests/float_full_time_widget_tests.js
@@ -1,35 +1,37 @@
+/* eslint no-undef: 0 */
 odoo.define('crnd_web_float_full_time_widget.tests', function (require) {
-"use strict";
+    "use strict";
 
-var FullFloatTime = require('crnd_web_float_full_time_widget.FullFloatTime');
+    var FullFloatTime = require(
+        'crnd_web_float_full_time_widget.FullFloatTime');
 
-QUnit.module('crnd_web_float_full_time_widget', {}, function () {
+    QUnit.module('crnd_web_float_full_time_widget', {}, function () {
 
-    QUnit.test('parse float full time', function(assert) {
-        assert.expect(7);
+        QUnit.test('parse float full time', function (assert) {
+            assert.expect(7);
 
-        assert.strictEqual(FullFloatTime.parse.float_full_time("", false, false), 0);
-        assert.strictEqual(FullFloatTime.parse.float_full_time("20.12", false, false), 20.12);
-        assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39,9999", true, false), 303648.999);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("", false, false), 0);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("20.12", false, false), 20.12);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39,9999", true, false), 303648.999);
 
-        assert.strictEqual(FullFloatTime.parse.float_full_time("3d 12:20:39,999", false, false), 303639.999);
-        assert.strictEqual(FullFloatTime.parse.float_full_time("3d 12:20:39", false, true), 303639);
-        assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39,999", true, false), 303639.999);
-        assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39", true, true), 303639);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("3d 12:20:39,999", false, false), 303639.999);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("3d 12:20:39", false, true), 303639);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39,999", true, false), 303639.999);
+            assert.strictEqual(FullFloatTime.parse.float_full_time("84:20:39", true, true), 303639);
 
+        });
+
+        QUnit.test('format float full time', function (assert) {
+            assert.expect(5);
+
+            assert.strictEqual(FullFloatTime.format.float_full_time(0, false, false), "0d 00:00:00,000");
+
+            assert.strictEqual(FullFloatTime.format.float_full_time(303639.999, false, false), "3d 12:20:39,999");
+            assert.strictEqual(FullFloatTime.format.float_full_time(303639, false, true), "3d 12:20:39");
+            assert.strictEqual(FullFloatTime.format.float_full_time(303639.999, true, false), "84:20:39,999");
+            assert.strictEqual(FullFloatTime.format.float_full_time(303639, true, true), "84:20:39");
+
+        });
     });
-
-    QUnit.test('format float full time', function(assert) {
-        assert.expect(5);
-
-        assert.strictEqual(FullFloatTime.format.float_full_time(0, false, false), "0d 00:00:00,000");
-
-        assert.strictEqual(FullFloatTime.format.float_full_time(303639.999, false, false), "3d 12:20:39,999");
-        assert.strictEqual(FullFloatTime.format.float_full_time(303639, false, true), "3d 12:20:39");
-        assert.strictEqual(FullFloatTime.format.float_full_time(303639.999, true, false), "84:20:39,999");
-        assert.strictEqual(FullFloatTime.format.float_full_time(303639, true, true), "84:20:39");
-
-    });
-});
 
 });
