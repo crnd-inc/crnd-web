cbf3f025bdc31c54b11186eb450f613f10f59037
Ihor <bezoblachnyj@gmail.com>
2019-05-03 19:32:15 +0300

    First working version.

---
+++ crnd_web_diagram_plus/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'CRND Web Diagram Plus',
+    'category': 'Hidden',
+    'description': """
+        Odoo Web Diagram view from CRND.
+    """,
+    'author': "Center of Research & Development",
+    'website': "https://crnd.pro",
+    'version': '11.0.0.0.1',
+    'depends': ['web'],
+    'data': [
+        'views/web_diagram_plus_templates.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'auto_install': True,
+}
---
+++ crnd_web_diagram_plus/models/ir_ui_view.py
@@ -0,0 +1,100 @@
+import itertools
+
+from operator import itemgetter
+from lxml import etree
+
+from odoo import models, fields, api, _
+
+
+class IrUiView(models.Model):
+
+    _inherit = 'ir.ui.view'
+
+    type = fields.Selection(selection_add=[('diagram_plus', 'DiagramPlus')])
+
+    @api.model
+    def postprocess_and_fields(self, model, node, view_id):
+        """
+            Rewrited for diagram_plus.
+            Almost Copy+Paste+Edit of original Odoo's method.
+        """
+        if node.tag != 'diagram_plus':
+            return super(IrUiView, self).postprocess_and_fields(
+                model, node, view_id)
+
+        fields = {}
+        color_fields = {}
+        if model not in self.env:
+            self.raise_view_error(
+                _('Model not found: %(model)s') % dict(model=model), view_id)
+        Model = self.env[model]
+
+        is_base_model = self.env.context.get('base_model_name', model) == model
+
+        if node.getchildren()[0].tag == 'node':
+            node_model = self.env[node.getchildren()[0].get('object')]
+            node_fields = node_model.fields_get(None)
+            # we have to get the fields of color attributes if assigned
+            color_fields = node_model.fields_get([
+                node.getchildren()[0].get('bg_color_field'),
+                node.getchildren()[0].get('fg_color_field'),
+            ])
+            fields.update(color_fields)
+            fields.update(node_fields)
+            if (not node.get("create") and
+                    not node_model.check_access_rights(
+                        'create', raise_exception=False) or
+                    not self._context.get("create", True) and
+                    is_base_model):
+                node.set("create", 'false')
+        if node.getchildren()[1].tag == 'arrow':
+            arrow_fields = self.env[
+                node.getchildren()[1].get('object')].fields_get(None)
+            fields.update(arrow_fields)
+
+        node = self.add_on_change(model, node)
+
+        attrs_fields = []
+        if self.env.context.get('check_field_names'):
+            editable = self.env.context.get('view_is_editable', True)
+            attrs_fields = self.get_attrs_field_names(node, Model, editable)
+
+        fields_def = self.postprocess(model, node, view_id, False, fields)
+        arch = etree.tostring(node, encoding="unicode").replace('\t', '')
+        for k in list(fields):
+            if k not in fields_def:
+                del fields[k]
+        # Ensure there are color fields
+        for c_f in color_fields:
+            if c_f not in fields:
+                message = _(
+                    "Field `%(field_name)s` must be present in"
+                    " diagram_plus[node][field], because it present in"
+                    " diagram_plus[node], but is missing."
+                ) % dict(field_name=c_f)
+                self.raise_view_error(message, view_id)
+        for field in fields_def:
+            if field in fields:
+                fields[field].update(fields_def[field])
+            else:
+                message = _(
+                    "Field `%(field_name)s` does not exist"
+                ) % dict(field_name=field)
+                self.raise_view_error(message, view_id)
+
+        missing = [item for item in attrs_fields if item[0] not in fields]
+        if missing:
+            msg_lines = []
+            msg_fmt = _("Field %r used in attributes must be"
+                        " present in view but is missing:")
+            line_fmt = _(" - %r in %s=%r")
+            for name, lines in itertools.groupby(
+                    sorted(missing), itemgetter(0)):
+                if msg_lines:
+                    msg_lines.append("")
+                msg_lines.append(msg_fmt % name)
+                for line in lines:
+                    msg_lines.append(line_fmt % line)
+            self.raise_view_error("\n".join(msg_lines), view_id)
+
+        return arch, fields
