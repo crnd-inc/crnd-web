cb8e2088904560c0f885914b1cb49ddc59448ce4
Ihor <bezoblachnyj@gmail.com>
2019-05-12 23:03:45 +0300

    Small fix by stylelint.

--- crnd_web_diagram_plus/models/ir_act_window_view.py
+++ crnd_web_diagram_plus/models/ir_act_window_view.py
@@ -4,4 +4,5 @@ from odoo import fields, models
 class IrActionsActWindowView(models.Model):
     _inherit = 'ir.actions.act_window.view'
 
-    view_mode = fields.Selection(selection_add=[('diagram_plus', 'DiagramPlus')])
+    view_mode = fields.Selection(
+        selection_add=[('diagram_plus', 'DiagramPlus')])
