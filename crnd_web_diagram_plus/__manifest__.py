c0c1bb7a15aa70cf51b58c0e59ebc761edf3b0b7
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2019-05-13 15:38:39 +0300

    crnd_web_diagram_fix: bump version

--- crnd_web_diagram_plus/__manifest__.py
+++ crnd_web_diagram_plus/__manifest__.py
@@ -6,7 +6,7 @@
     """,
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
-    'version': '11.0.0.0.2',
+    'version': '11.0.0.0.3',
     'depends': ['web'],
     'data': [
         'views/web_diagram_plus_templates.xml',
