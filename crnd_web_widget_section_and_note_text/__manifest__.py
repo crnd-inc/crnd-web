6b6055a17da9ea0374ba6ecf15dbdc3b72cbf623
Bezobiuk Ihor <bepzoblachnyj@gmail.com>
2020-04-17 23:07:06 +0300

    Added crnd_web_widget_section_and_note_text addon.

---
+++ crnd_web_widget_section_and_note_text/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "CRND Web Widget Section And Note Text",
+    "version": "13.0.0.0.1",
+    "author": "Center of Research and Development",
+    "website": "https://crnd.pro",
+    'summary': 'Makes the standard section_and_note_text widget compatible '
+               'with CRND Web List Popover Widget.',
+    "license": "LGPL-3",
+    'category': 'Technical Settings',
+    'depends': [
+        'web',
+        'crnd_web_list_popover_widget',
+        'account',
+    ],
+    'data': [
+        'views/assets_backend.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ crnd_web_widget_section_and_note_text/static/src/js/crnd_web_section_and_note_text.js
@@ -0,0 +1,22 @@
+
+odoo.define('crnd_web_widget_section_and_note_text.crnd_section_and_note_text',
+ function (require) {
+	"use strict";
+	var FieldChar = require('web.basic_fields').FieldChar;
+	var CRNDListFieldText = require(
+		'crnd_web_list_popover_widget.DynamicTextPopover');
+	var fieldRegistry = require('web.field_registry');
+
+	// This is a merge between a CRNDListFieldText and a CRNDFieldChar.
+	// We want a FieldChar for section,
+	// and a CRNDListFieldText for the rest (product and note).
+	var CRNDSectionAndNoteFieldText = function (parent, name, record, options) {
+	    var isSection = record.data.display_type === 'line_section';
+	    var Constructor = isSection ? FieldChar : CRNDListFieldText;
+	    return new Constructor(parent, name, record, options);
+	};
+
+	fieldRegistry.add(
+		'crnd_section_and_note_text', CRNDSectionAndNoteFieldText);
+
+});
