4eae0a88c170b0f656692fdf423c6f6a8f37a96d
Nasalskyi Olexandr <olexandrfec@gmail.com>
2020-05-08 16:07:55 +0300

    Refactoring code

--- crnd_web_tree_colored_field/static/src/js/crnd_web_tree_colored_field.js
+++ crnd_web_tree_colored_field/static/src/js/crnd_web_tree_colored_field.js
@@ -6,7 +6,6 @@ odoo.define('crnd_web_tree_colored_field', function (require) {
 
         _renderBodyCell: function (record, node) {
             var $td = this._super.apply(this, arguments);
-            var bodyOptions = node.attrs.options;
 
             if (!node.attrs.options) {
                 return $td;
@@ -14,25 +13,16 @@ odoo.define('crnd_web_tree_colored_field', function (require) {
             if (node.tag !== 'field') {
                 return $td;
             }
-            var splited_options = bodyOptions.split(',');
+            var nodeOptions = JSON.parse((node.attrs || {})
+                .options.replace(/'/g, '"') || "{}");
 
-            for (var i=0, len=splited_options.length; i<len; ++i) {
-                var pair = splited_options[i];
-                pair = pair.replace('{', '');
-                pair = pair.replace('}', '');
-                pair = pair.split(':');
-                var param = pair[0].trim();
-                var value = pair[1].trim();
-                value = value.replace("'", '');
-                value = value.replace("'", '');
-                if (param === "'field_bg_color'") {
-                    var bg_color = record.data[value];
-                    $td.css('background-color', bg_color);
-                }
-                if (param === "'field_label_color'") {
-                    var label_color = record.data[value];
-                    $td.css('color', label_color);
-                }
+            if (nodeOptions.field_bg_color) {
+                var bg_color = record.data[nodeOptions.field_bg_color];
+                $td.css('background-color', bg_color);
+            }
+            if (nodeOptions.field_label_color) {
+                var label_color = record.data[nodeOptions.field_label_color];
+                $td.css('color', label_color);
             }
             return $td;
         },
