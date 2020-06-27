c597a52fc00d33fcf51ff4318f5170ff4d5a6b39
Ostap Yakovenko <ceo@crnd.pro>
2020-06-27 10:03:28 +0300

    Add Web Client Extension

--- crnd_web_client/__manifest__.py
+++ crnd_web_client/__manifest__.py
@@ -3,7 +3,7 @@
 #  See LICENSE file for licensing details.
 {
     'name': 'CRnD Web Client',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'summary': 'Web Client Extention',
     'category': 'Tools',
     'author': 'Center of Research and Development ',
