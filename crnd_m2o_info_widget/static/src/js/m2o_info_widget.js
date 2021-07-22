3f23aa207667aaa4a6e50403f93c6edec31aa5d2
Dmytro Katyukha <cto@crnd.pro>
2021-07-22 17:58:09 +0300

    crnd_m2o_info_widget: Refactored for better readability
    Logic slpited by several simple methods

--- crnd_m2o_info_widget/static/src/js/m2o_info_widget.js
+++ crnd_m2o_info_widget/static/src/js/m2o_info_widget.js
@@ -10,7 +10,8 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
             this._super.apply(this, arguments);
 
             if (this.m2o_value) {
-                this.info_data = false;
+                this.info_data = null;
+                this.$info_popup = null;
 
                 var $link = this.$el;
                 this.$el = $('<div>').addClass('m2o_info');
@@ -26,82 +27,143 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
             }
         },
 
-        _onClickInfo: function (event) {
-            if (this.info_data) {
-                this._showInfoPopup(true, event.clientX);
+        /**
+         * Get Data for this widget.
+         * @returns {Deferred}
+         */
+        _getInfoData: function () {
+            var self = this;
+            var def = $.Deferred();
+            if (self.info_data) {
+                def.resolve(self.info_data);
             } else {
-                var self = this;
-
-                this._rpc({
-                    model: this.value.model,
+                self._rpc({
+                    model: self.value.model,
                     method: 'read',
-                    args: [[this.value.res_id], this.nodeOptions.info_fields],
+                    args: [[self.value.res_id], self.nodeOptions.info_fields],
                 }).then(function (result) {
-                    self._createInfoPopup(result, self.nodeOptions.info_fields);
-                    self.info_data = true;
-                    self._showInfoPopup(true, event.clientX);
+                    if (result) {
+                        self.info_data = result[0];
+                        def.resolve(self.info_data);
+                    } else {
+                        self.info_data = null;
+                        def.resolve(null);
+                    }
                 });
             }
+            return def;
+        },
+
+        /**
+         * Create popup window for this widget.
+         * @param {Object} data: dictionary with fields read from database
+         * @returns JQuery element of created popup
+         */
+        _createInfoPopup: function (data) {
+            var self = this;
+
+            self.$info_popup = $('<div>').addClass('info_popup')
+                .appendTo(self.$info_icon);
+
+            if (data) {
+                _.each(
+                    self.nodeOptions.info_fields,
+                    function (field_name) {
+                        if (data[field_name]) {
+                            // TODO: possibly use template to build this widget
+                            //       Also, it would be good to display field
+                            //       names left side field value.
+                            //       For example:
+                            //           Name: John Doe
+                            //           Phone: 12335
+                            //           Email: john.doe@jd.com
+                            var $row = $('<div>').appendTo(self.$info_popup);
+                            $('<label>').text(data[field_name]).appendTo($row);
+                            var $copy = $('<span>').addClass('fa fa-copy')
+                                .appendTo($row);
+
+                            // TODO: implement in better way
+                            new ClipboardJS($copy[0], {  // eslint-disable-line
+                                text: function (target) {
+                                    return target.previousElementSibling
+                                        .textContent;
+                                },
+                            });
+                        }
+                    });
+            }
+            return self.$info_popup;
+        },
+
+        /**
+         * Get or create (if needed) popup window for this widget.
+         * @returns {Deferred}
+         */
+        _getOrCreateInfoPopup: function () {
+            var self = this;
+
+            var def = $.Deferred();
+
+            if (self.$info_popup) {
+                def.resolve(self.$info_popup);
+            } else {
+                self._getInfoData().then(function (data) {
+                    def.resolve(self._createInfoPopup(data));
+                });
+            }
+            return def;
+        },
+
+        /**
+         * Show info popup.
+         * @param {Number} left_offset: Have to be event.clientX of click event
+         */
+        _showInfoPopup: function (left_offset) {
+            var self = this;
+            self._getOrCreateInfoPopup().then(function ($info_popup) {
+                if (!$info_popup.hasClass('info_popup_visible')) {
+                    var popup_width = $info_popup.outerWidth();
+                    var window_width = $(window).width();
+                    var right_offset = window_width - left_offset;
+                    var css_value = {};
+                    if (left_offset >= popup_width) {
+                        css_value = {
+                            right: '-1px',
+                            left: 'none',
+                        };
+                    } else if (left_offset <= popup_width &&
+                            right_offset >= popup_width) {
+                        css_value = {
+                            right: 'none',
+                            left: '-1px',
+                        };
+                    } else {
+                        css_value = {
+                            right: 'none',
+                            left: right_offset - popup_width - 10 + 'px',
+                        };
+                    }
+                    self.$info_popup.css(css_value);
+                    self.$info_popup.addClass('info_popup_visible');
+                }
+            });
+        },
+
+        _hideInfoPopup: function () {
+            var self = this;
+            self._getOrCreateInfoPopup().then(function ($info_popup) {
+                $info_popup.removeClass('info_popup_visible');
+            });
+        },
+
+        _onClickInfo: function (event) {
+            var self = this;
+            self._showInfoPopup(event.clientX);
         },
 
         _onMouseLeaveInfo: function () {
-            if (this.info_data) {
-                this._showInfoPopup(false);
-            }
-        },
-
-        _createInfoPopup: function (data, info_fields) {
-            this.$info_popup = $('<div>').addClass('info_popup')
-                .appendTo(this.$info_icon);
-
-            if (data) {
-                for (var i = 0; i < info_fields.length; i++) {
-                    if (data[0][info_fields[i]]) {
-                        var $row = $('<div>').appendTo(this.$info_popup);
-                        $('<label>')
-                            .text(data[0][info_fields[i]]).appendTo($row );
-                        var $copy = $('<span>').addClass('fa fa-copy')
-                            .appendTo($row);
-                        new ClipboardJS($copy[0], {  // eslint-disable-line
-                            text: function (target) {
-                                return target.previousElementSibling
-                                    .textContent;
-                            },
-                        });
-                    }
-                }
-            }
-        },
-
-        _showInfoPopup: function (state, left_offset) {
-            var is_show = this.$info_popup.hasClass('info_popup_visible');
-            if (!state && is_show) {
-                this.$info_popup.removeClass('info_popup_visible');
-            } else if (state && !is_show) {
-                var popup_width = this.$info_popup.outerWidth();
-                var window_width = $(window).width();
-                var right_offset = window_width - left_offset;
-                var css_value = {};
-                if (left_offset >= popup_width) {
-                    css_value = {
-                        right: '-1px',
-                        left: 'none',
-                    };
-                } else if (left_offset <= popup_width &&
-                           right_offset >= popup_width) {
-                    css_value = {
-                        right: 'none',
-                        left: '-1px',
-                    };
-                } else {
-                    css_value = {
-                        right: 'none',
-                        left: right_offset - popup_width - 10 + 'px',
-                    };
-                }
-                this.$info_popup.css(css_value);
-                this.$info_popup.addClass('info_popup_visible');
-            }
+            var self = this;
+            self._hideInfoPopup();
         },
     });
 
