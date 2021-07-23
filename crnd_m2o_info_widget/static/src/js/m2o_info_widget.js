97c0d45c5d8edfb10836f46910931e86dc110442
Dmytro Katyukha <cto@crnd.pro>
2021-07-23 17:17:58 +0300

    [REF] crmd_m2o_info_widget: User standard bootstrap popover, instead custom one

--- crnd_m2o_info_widget/static/src/js/m2o_info_widget.js
+++ crnd_m2o_info_widget/static/src/js/m2o_info_widget.js
@@ -7,6 +7,7 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
     var fieldMany2One = relationFields.FieldMany2One;
 
     var M2OInfo = fieldMany2One.extend({
+
         _renderReadonly: function () {
             this._super.apply(this, arguments);
 
@@ -20,20 +21,18 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
                 //     },
                 // }
                 this.info_data = null;
-                this.$info_popup = null;
+                this.popover_initialized = false;
 
                 var $link = this.$el;
                 this.$el = $('<div>').addClass('m2o_info');
                 this.$el.append($link);
 
                 this.$info_icon = $('<span>')
-                    .addClass('info_icon btn btn-sm btn-outline-primary ml4')
+                    .addClass('m2o-info-icon btn btn-sm btn-outline-primary ml4')
                     .appendTo(this.$el)
                     .append($('<i>').addClass('fa fa-info'));
 
                 this.$info_icon.on('click', this._onClickInfo.bind(this));
-                this.$info_icon.on('mouseleave',
-                    this._onMouseLeaveInfo.bind(this));
             }
         },
 
@@ -97,7 +96,13 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
             return def;
         },
 
-        _renderPopUpRow: function (data, field_name) {
+        /**
+         * Render single row of popover content.
+         * @param {Object} data: info_data object
+         * @param {String} field_name: name of field to render
+         * @returns JQuery element of created row
+         */
+        _renderInfoPopUpRow: function (data, field_name) {
             var $row = $('<tr>');
             $('<th>').text(
                 data[field_name].name + ":").appendTo($row);
@@ -113,27 +118,27 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
         },
 
         /**
-         * Create popup window for this widget.
+         * Render popover content for this widget.
          * @param {Object} data: info_data object
-         * @returns JQuery element of created popup
+         * @returns JQuery element of created popover
          */
-        _createInfoPopup: function (data) {
+        _renderInfoPopUp: function (data) {
             var self = this;
-
-            self.$info_popup = $('<div>').addClass('info_popup')
-                .appendTo(self.$info_icon);
+            var $info_popup = $('<div>').addClass('m2o-info-popover')
             var $info_table = $('<table>')
                 .addClass('table table-sm table-borderless')
-                .appendTo(self.$info_popup);
+                .appendTo($info_popup);
 
             if (data) {
                 _.each(
                     self.nodeOptions.info_fields,
                     function (field_name) {
                         if (data[field_name].value) {
-                            var $row = self._renderPopUpRow(data, field_name);
+                            var $row = self._renderInfoPopUpRow(
+                                data, field_name);
                             $row.appendTo($info_table);
 
+                            // Update data with generated elements
                             data[field_name].clipboard = new ClipboardJS(
                                 $row.find('.m2o-info-copy')[0],
                                 {
@@ -148,78 +153,33 @@ odoo.define('crnd_m2o_info_widget.widget', function (require) {
                         }
                     });
             }
-            return self.$info_popup;
-        },
-
-        /**
-         * Get or create (if needed) popup window for this widget.
-         * @returns {Deferred}
-         */
-        _getOrCreateInfoPopup: function () {
-            var self = this;
-
-            var def = $.Deferred();
-
-            if (self.$info_popup) {
-                def.resolve(self.$info_popup);
-            } else {
-                self._getInfoData().then(function (data) {
-                    def.resolve(self._createInfoPopup(data));
-                });
-            }
-            return def;
-        },
-
-        /**
-         * Show info popup.
-         * @param {Number} left_offset: Have to be event.clientX of click event
-         */
-        _showInfoPopup: function (left_offset) {
-            var self = this;
-            self._getOrCreateInfoPopup().then(function ($info_popup) {
-                if (!$info_popup.hasClass('info_popup_visible')) {
-                    var popup_width = $info_popup.outerWidth();
-                    var window_width = $(window).width();
-                    var right_offset = window_width - left_offset;
-                    var css_value = {};
-                    if (left_offset >= popup_width) {
-                        css_value = {
-                            right: '-1px',
-                            left: 'none',
-                        };
-                    } else if (left_offset <= popup_width &&
-                            right_offset >= popup_width) {
-                        css_value = {
-                            right: 'none',
-                            left: '-1px',
-                        };
-                    } else {
-                        css_value = {
-                            right: 'none',
-                            left: right_offset - popup_width - 10 + 'px',
-                        };
-                    }
-                    self.$info_popup.css(css_value);
-                    self.$info_popup.addClass('info_popup_visible');
-                }
-            });
-        },
-
-        _hideInfoPopup: function () {
-            var self = this;
-            self._getOrCreateInfoPopup().then(function ($info_popup) {
-                $info_popup.removeClass('info_popup_visible');
-            });
+            return $info_popup;
         },
 
         _onClickInfo: function (event) {
             var self = this;
-            self._showInfoPopup(event.clientX);
-        },
+            var def = $.Deferred();
+            if (!self.popover_initialized) {
+                self._getInfoData().then(function (data) {
+                    var $info_popup = self._renderInfoPopUp(data);
+                    self.$info_icon.popover({
+                        content: $info_popup,
+                        html: true,
+                        trigger: 'manual',
+                        animation: true,
+                    });
+                    $info_popup.on('mouseleave', function () {
+                        self.$info_icon.popover('hide');
+                    });
+                    def.resolve();
+                })
+            } else {
+                def.resolve();
+            }
 
-        _onMouseLeaveInfo: function () {
-            var self = this;
-            self._hideInfoPopup();
+            def.then(function () {
+                self.$info_icon.popover('toggle');
+            });
         },
     });
 
