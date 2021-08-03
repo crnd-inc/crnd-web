881fdce3446a4befaa9059f75fe79b7d57fe0f13
rymar.roman <rymar.roman@crnd.int>
2021-08-03 10:08:39 +0300

    solving problem with hide popover refactoring

--- crnd_web_m2o_info_widget/static/src/js/form_controller.js
+++ crnd_web_m2o_info_widget/static/src/js/form_controller.js
@@ -5,13 +5,11 @@ odoo.define('web.M2OInfoWidgetFormController', function (require) {
 
     FormController.include({
         on_detach_callback: function () {
-            if (this.modelName === 'request.request') {
-                $.each($('body').children(), function (index, value) {
-                    if (value.id && value.id.indexOf('popover') >= 0) {
-                        value.remove();
-                    }
-                });
-            }
+            $.each($('body').children(), function (index, value) {
+                if (value.id && value.id.indexOf('popover') === 0) {
+                    value.remove();
+                }
+            });
             this._super.apply(this, arguments);
         },
     });
