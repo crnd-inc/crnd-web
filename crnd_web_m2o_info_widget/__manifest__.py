ffa11442b346fcfb8cfb4aa306ebdd5998f61fb0
rymar.roman <rymar.roman@crnd.pro>
2021-12-07 17:20:48 +0200

    crnd_web_m2o_info_widget change popover template

--- crnd_web_m2o_info_widget/__manifest__.py
+++ crnd_web_m2o_info_widget/__manifest__.py
@@ -20,4 +20,3 @@
     'application': False,
     'auto_install': False,
 }
-'<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
