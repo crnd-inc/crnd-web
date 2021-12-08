66279721a4cbcd92a35fcd3a07cfcff19d38b1f7
rymar.roman <rymar.roman@crnd.pro>
2021-12-08 10:50:56 +0200

    crnd_web_button_box_full_width porting to v15

--- crnd_web_button_box_full_width/__manifest__.py
+++ crnd_web_button_box_full_width/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "CRND Web Button Box Fullwidth",
-    "version": "14.0.0.1.1",
+    "version": "15.0.0.1.1",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     'summary': 'Button_box at the top of the form',
@@ -9,9 +9,11 @@
     'depends': [
         'web',
     ],
-    'data': [
-        'views/assets.xml',
-    ],
+    'assets': {
+        'web.assets_backend': [
+            'crnd_web_button_box_full_width/static/src/scss/style.scss',
+        ],
+    },
     'images': ['static/description/banner.gif'],
     'installable': True,
     'auto_install': False,
