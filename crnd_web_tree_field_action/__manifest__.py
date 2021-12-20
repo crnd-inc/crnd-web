48cfebf13154c9487e6e4204cf9d442faa47ce6f
rymar.roman <rymar.roman@crnd.pro>
2021-12-20 16:56:45 +0200

    add module crnd_web_tree_field_action

---
+++ crnd_web_tree_field_action/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'CRND web tree field action',
+    'version': '12.0.0.1.0',
+    'author': "Center of Research and Development",
+    'website': "https://crnd.pro",
+    'summary': """""",
+    'license': 'LGPL-3',
+    'category': 'Technical Settings',
+
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
