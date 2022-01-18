c8cc0ff0c18b0ac213a3dd36a2b00d40b274f2eb
rymar.roman <rymar.roman@crnd.pro>
2022-01-18 15:15:07 +0200

    crnd_web_action_nothing refactoring

--- crnd_web_action_nothing/static/src/js/action_manager.js
+++ crnd_web_action_nothing/static/src/js/action_manager.js
@@ -1,12 +1,12 @@
-odoo.define("crnd_web_action_nothing.ActionManager", function(require) {
+odoo.define("crnd_web_action_nothing.ActionManager", function (require) {
     "use strict";
 
     require('web.ActionManager').include({
         // Simple action that do nothing.
         // This is required to prevent closing wizard window
-        _handleAction: function(action, options) {
+        _handleAction: function (action) {
             if (action.type === "ir.actions.nothing") {
-                return Promise.resolve();
+                return $.Deferred().resolve();
             }
             return this._super.apply(this, arguments);
         },
