46fcfa6f187bb23ee5431eb94f2d323096508281
rymar.roman <rymar.roman@crnd.pro>
2022-01-20 08:19:32 +0200

    create module crnd_web_action_reload

---
+++ crnd_web_action_reload/models/ir_actions_reload.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class IrActionsReload(models.Model):
+    _name = 'ir.actions.reload'
+    _inherit = 'ir.actions.actions'
---
+++ crnd_web_action_reload/static/src/js/action_manager.js
@@ -0,0 +1,24 @@
+odoo.define("crnd_web_action_reload.ActionManager", function (require) {
+    "use strict";
+
+    require('web.ActionManager').include({
+
+        _handleAction: function (action) {
+            if (action.type === "ir.actions.reload") {
+                var actModel = action.context.active_model;
+                var currentController = this.getCurrentController();
+                var currentDialogController =
+                    this.getCurrentControllerInDialog();
+
+                if (currentDialogController.widget.modelName === actModel) {
+                    return currentDialogController.widget.reload();
+                } else if (currentController.widget.modelName === actModel) {
+                    return currentController.widget.reload();
+                }
+
+                return $.Deferred().resolve();
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+});
