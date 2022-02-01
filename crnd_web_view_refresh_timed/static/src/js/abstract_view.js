7a6c6cc274412febe0690497b12aafcbc5818355
rymar.roman <rymar.roman@crnd.pro>
2022-02-01 13:20:36 +0200

    create module crnd_web_view_refresh_timed

---
+++ crnd_web_view_refresh_timed/static/src/js/abstract_view.js
@@ -0,0 +1,31 @@
+odoo.define('crnd_web_view_refresh_timed.AbstractView', function (require) {
+    "use strict";
+
+    require('web.AbstractView').include({
+
+        init: function () {
+            this._super.apply(this, arguments);
+
+            var self = this;
+            if (this.arch.attrs.class) {
+                var classList = this.arch.attrs.class.split(' ');
+                var refreshClass = 'crnd-refresh-every-';
+                classList.forEach(function (className) {
+                    if (className.includes(refreshClass)) {
+                        var time = className.substring(refreshClass.length,);
+                        var interval = time.slice(0, -1);
+                        var dimension = time.slice(-1);
+                        if ((dimension === 's' || dimension === 'm') &&
+                            /^\d+$/.test(interval)) {
+                            var dCoefficient = 1;
+                            if (dimension === 'm') dCoefficient = 60;
+                            self.controllerParams.crndViewRefreshInterval =
+                                parseInt(interval) * dCoefficient * 1000;
+                        }
+                    }
+                });
+            }
+        },
+
+    });
+});
