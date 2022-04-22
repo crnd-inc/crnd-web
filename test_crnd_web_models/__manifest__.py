99fc883ac669cf6e4f33b23b7d7d649fdcb3f1ae
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-04-22 17:05:15 +0300

    m2o_info widget: Added ability to call model's method to get info to display

--- test_crnd_web_models/__manifest__.py
+++ test_crnd_web_models/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Test CRND Web Models",
-    "version": "12.0.0.4.0",
+    "version": "12.0.0.5.0",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     'summary': 'Module for testing web addons.',
