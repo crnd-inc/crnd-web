29d5143fc4af25e58d4b6e2c2970d2fa4ef1e28e
AShurikas <ANechipor@gmail.com>
2022-07-11 16:12:10 +0300

    fix linter

--- test_crnd_web_models/static/src/js/tours/test_crnd_web_actions.js
+++ test_crnd_web_models/static/src/js/tours/test_crnd_web_actions.js
@@ -19,7 +19,8 @@ odoo.define('test_crnd_web_models.test_crnd_web_actions', function (require) {
         },
         {
             content: "Click menu 'CRND Web Tests'",
-            trigger: "a[data-menu-xmlid='test_crnd_web_models.crnd_web_tests']"
+            trigger: "a[data-menu-xmlid=" +
+                "'test_crnd_web_models.crnd_web_tests']",
         },
         {
             content: "Click menu 'Test crnd_web_actions'",
@@ -28,7 +29,8 @@ odoo.define('test_crnd_web_models.test_crnd_web_actions', function (require) {
         },
         {
             content: "Wait for download",
-            trigger: "div[class='o_control_panel'] ol:has(li:containsExact('Actions'))",
+            trigger: "div[class='o_control_panel'] ol:has" +
+                "(li:containsExact('Actions'))",
         },
         {
             content: "Click create button",
