cfd8ca040605a026470f8b2eaa0adb12206f29e5
Oleksandr Nasalskyi <olexandrfec@gmail.com>
2023-10-31 18:30:54 +0200

    Improve icons

--- crnd_web_field_domain/__manifest__.py
+++ crnd_web_field_domain/__manifest__.py
@@ -8,7 +8,7 @@
     'support': 'info@crnd.pro',
     'website': 'https://crnd.pro',
     'license': 'LGPL-3',
-    'version': '12.0.0.3.0',
+    'version': '12.0.0.4.0',
     'depends': [
         'web',
     ],
