8eeccab79b91144defb806959a6b3dafe0057213
Oleksandr Nasalskyi <nasalskyi@maao.int>
2023-10-31 16:35:34 +0000

    Fix version

--- crnd_web_m2o_info_widget/__manifest__.py
+++ crnd_web_m2o_info_widget/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'license': 'LGPL-3',
-    'version': '12.0.0.1.0',
+    'version': '12.0.0.10.0',
 
     'depends': [
         'web',
