472e5b10b3084624047ea5c84566005da388ddf8
Oleksandr Nechyporenko <a.nechipor@crnd.int>
2023-11-21 17:14:15 +0200

    Improve colorizer to use expressions

--- crnd_web_tree_colored_field/static/src/js/crnd_web_tree_colored_field.js
+++ crnd_web_tree_colored_field/static/src/js/crnd_web_tree_colored_field.js
@@ -24,7 +24,73 @@ odoo.define('crnd_web_tree_colored_field', function (require) {
                 var label_color = record.data[nodeOptions.field_label_color];
                 $td.css('color', label_color);
             }
+            if (nodeOptions.field_label_color_expression) {
+                var expression = nodeOptions.field_label_color_expression;
+                var label_color = this._getColorBasedOnExpression(record.data, expression);
+                $td.css('color', label_color);
+            }
+            if (nodeOptions.field_bg_color_expression) {
+                var expression = nodeOptions.field_bg_color_expression;
+                var bg_color = this._getColorBasedOnExpression(record.data, expression)
+                $td.css('background-color', bg_color);
+            }
             return $td;
         },
+        _getColorBasedOnExpression: function (obj, expr) {
+            // Split the expression into conditions
+            var conditions = expr.split(';');
+
+            // Iterate through conditions
+            for (var i = 0; i < conditions.length; i++) {
+                var condition = conditions[i].trim();
+
+                // Split each condition into color and expression
+                var [color, statement] = condition.split(':');
+
+                // Remove leading and trailing spaces from the statement
+                statement = statement.trim();
+
+                // Evaluate the expression
+                if (this._checkCondition(obj, statement)) {
+                    return color;
+                }
+            }
+
+            // Default color if no condition is met
+            return '$table-dark-color';
+        },
+        _checkCondition: function (obj, statement) {
+
+            // Split the statement into property, operator, and value
+            var [property, operator, value] = statement.split(/(===|!==|==|!=)/);
+
+            // Remove leading and trailing spaces
+            property = property.trim();
+            operator = operator.trim();
+            value = value.trim();
+
+            // Remove both single and double quotes around the value if present
+            value = value.replace(/^['"]|['"]$/g, '');
+
+            // Check if the property exists and evaluate the condition based on the operator
+            if (obj.hasOwnProperty(property)) {
+                switch (operator) {
+                    case '===':
+                        return obj[property] === value;
+                    case '!==':
+                        return obj[property] !== value;
+                    case '==':
+                        return obj[property] == value;
+                    case '!=':
+                        return obj[property] != value;
+                    default:
+                        // Default to '===' if the operator is not recognized
+                        return obj[property] === value;
+                }
+            }
+
+            // Return false if the property doesn't exist
+            return false;
+        }
     });
 });
