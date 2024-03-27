77767439bc9661e56508928686db3a5101da794b
Tymur Ivasiuk <ivasiuktymur@gmail.com>
2024-03-27 16:27:21 +0200

    [FIX] crnd_web_on_create_action: fix version

--- crnd_web_on_create_action/__manifest__.py
+++ crnd_web_on_create_action/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRND web on create action',
-    'version': '17.0.0.6.0',
+    'version': '17.0.0.5.1',
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'summary': "Make it possible to use wizards to create records",
