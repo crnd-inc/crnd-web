d1c21eac18a8667c8c03a6684466e5f46d666a0f
Oleksandr Nechyporenko <ANechipor@gmail.com>
2024-03-28 08:36:58 +0200

    Activate test tree colored

--- test_crnd_web_models/models/__init__.py
+++ test_crnd_web_models/models/__init__.py
@@ -2,7 +2,7 @@ from . import (
     # float_full_time_widget,
     # float_time_duration_widget,
     # m2o_info_widget,
-    # tree_colored_field,
+    tree_colored_field,
     # web_diagram_plus,
     # res_partner,
     test_crnd_web_model_book,
