1b61def2ba558bbef54d75033f4fa5af250f39b0
Oleksandr Nechyporenko <ANechipor@gmail.com>
2026-01-11 18:18:30 +0200

    Port module

--- crnd_web_m2o_info_widget/static/src/js/m2o_info_widget.js
+++ crnd_web_m2o_info_widget/static/src/js/m2o_info_widget.js
@@ -1,297 +1,156 @@
 /** @odoo-module **/
 
-import fieldRegistry from 'web.field_registry';
-import { FieldMany2One } from 'web.relational_fields';
-import { qweb } from 'web.core';
+import { browser } from "@web/core/browser/browser";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { Many2OneField } from "@web/views/fields/many2one/many2one_field";
+import { usePopover } from "@web/core/popover/popover_hook";
 
-var M2OInfo = FieldMany2One.extend({
+import { Component, useRef, useState } from "@odoo/owl";
 
-    init: function () {
-        var self = this;
-        self._super.apply(self, arguments);
+class M2OInfoPopover extends Component {
+    setup() {
+        this.notification = useService("notification");
+    }
 
-        self.popover_initialized = false;
-
-        // Keep popover info, to be able to destroy when widget
-        // desttroyed
-        self.popover_data = null;
-        self.popover_field_data = null;
-    },
-
-    _renderReadonly: function () {
-        this._super.apply(this, arguments);
-
-        if (this.m2o_value) {
-            // Format is info_data = {
-            //     field_name: {
-            //         name: name of field
-            //         value: value of field
-            //         $el: jquery element of the field
-            //         clipboard: ClipboardJS instance
-            //     },
-            // }
-
-            var $link = this.$el;
-            this.$el = $('<div>').addClass('m2o_info');
-            this.$el.append($link);
-
-            this.$info_icon = $('<span>')
-                .addClass(
-                    'm2o-info-icon btn btn-sm btn-outline-primary ml4')
-                .appendTo(this.$el)
-                .append($('<i>').addClass('fa fa-info'));
-
-            this.$info_icon.on('click', this._onClickInfo.bind(this));
-        }
-    },
-
-    /* Get info data based on fields specified in nodeOptions.info_fields
-    *
-    * @returns {Deferred} array of objects. Each object represents info
-    * about single field.
-    *
-    * Example of return result:
-    * [
-    *     {
-    *          name: name of the field,
-    *          string: label of the field,
-    *          value: value of the field,
-    *     },
-    * ]
-    */
-    _getInfoDataFields: function () {
-        var self = this;
-        var def = $.Deferred();
-
-        // Initialize info data
-        var info_data = {};
-        _.each(
-            self.nodeOptions.info_fields,
-            function (field_name) {
-                info_data[field_name] = {};
+    async copy(value) {
+        if (!browser.navigator.clipboard) {
+            this.notification.add(this.env._t("This browser doesn't allow to copy to clipboard"), {
+                type: "warning",
             });
-
-        // Fill values for each field
-        var def_field_values = self._rpc({
-            model: self.value.model,
-            method: 'read',
-            args: [[self.value.res_id], self.nodeOptions.info_fields],
-        }).then(function (result) {
-            _.each(
-                self.nodeOptions.info_fields,
-                function (field_name) {
-                    if (result) {
-                        var val = result[0][field_name];
-                        info_data[field_name].value = val;
-                    } else {
-                        info_data[field_name].value = '';
-                    }
-                });
-        });
-
-        // Get field names
-        var def_field_names = self._rpc({
-            model: self.value.model,
-            method: 'fields_get',
-            args: [self.nodeOptions.info_fields, ['string']],
-        }).then(function (result) {
-            _.each(
-                self.nodeOptions.info_fields,
-                function (field_name) {
-                    if (result) {
-                        var val = result[field_name].string;
-                        info_data[field_name].name = val;
-                    } else {
-                        info_data[field_name].name = '';
-                    }
-                });
-        });
-        $.when(def_field_values, def_field_names).then(function () {
-            var res = [];
-            _.each(
-                self.nodeOptions.info_fields,
-                function (field_name) {
-                    res.push({
-                        name: field_name,
-                        string: info_data[field_name].name,
-                        value: info_data[field_name].value,
-                    });
-                });
-            def.resolve(res);
-        });
-        return def;
-    },
-
-    /* Get info data based on method called on field's relation model.
-    *
-    * @returns {Deferred} array of objects.
-    * Each object represents info about single field.
-    *
-    * Example of return result:
-    * [
-    *     {
-    *          name: name of the field,
-    *          string: label of the field,
-    *          value: value of the field,
-    *     },
-    * ]
-    */
-    _getInfoDataMethod: function () {
-        var self = this;
-        return self._rpc({
-            model: self.value.model,
-            method: self.nodeOptions.info_method,
-            args: [[self.value.res_id]],
-        });
-    },
-
-    /* Get info data based on fields specified in nodeOptions.info_fields
-    *
-    * @returns {Deferred} array of objects.
-    * Each object represents info about single field.
-    *
-    * Example of return result:
-    * [
-    *     {
-    *          name: name of the field,
-    *          string: label of the field,
-    *          value: value of the field,
-    *     },
-    * ]
-    */
-    _getInfoData: function () {
-        var self = this;
-
-        if (self.nodeOptions.info_fields) {
-            return self._getInfoDataFields();
-        } else if (self.nodeOptions.info_method) {
-            return self._getInfoDataMethod();
+            return;
         }
-        console.log(
-            "Cannot many2one field info. Field is not configured.");
-    },
-
-    /**
-     * Render single row of popover content.
-     * @param {Object} field_info: info about single field
-     * @param {String} field_name: name of field to render
-     * @returns JQuery element of created row
-     */
-    _renderInfoPopUpRow: function (field_info) {
-        var $row = $('<tr>');
-        $('<th>').text(field_info.string + ":")
-            .addClass('info_label')
-            .appendTo($row);
-        $('<td>').text(
-            field_info.value).appendTo($row);
-        var $copy_cell = $('<td>').appendTo($row);
-        var $copy = $('<span>')
-            .addClass('m2o-info-copy btn btn-sm btn-outline-primary')
-            .data('field-name', field_info.name)
-            .appendTo($copy_cell);
-        $('<i/>').addClass('fa fa-copy').appendTo($copy);
-        return $row;
-    },
-
-    /**
-     * Render popover content for this widget.
-     * @param {Object} data: info_data object
-     * @returns JQuery element of created popover
-     */
-    _renderInfoPopUp: function (data) {
-        var self = this;
-        var $info_popup = $('<div>').addClass('m2o-info-popover');
-        var $info_table = $('<table>')
-            .addClass('table table-sm table-borderless')
-            .appendTo($info_popup);
-
-        if (data) {
-            _.each(data, function (field_info) {
-                if (field_info.value) {
-                    var $row = self._renderInfoPopUpRow(field_info);
-                    $row.appendTo($info_table);
-
-                    // Update data with generated elements
-                    field_info.clipboard = new ClipboardJS(
-                        $row.find('.m2o-info-copy')[0],
-                        {
-                            text: function () {
-                                return field_info.value;
-                            },
-                        }
-                    );
-                    field_info.$el = $row;
-                }
-            });
+        try {
+            await browser.navigator.clipboard.writeText(String(value ?? ""));
+            this.notification.add(this.env._t("Copied"), { type: "success" });
+        } catch (error) {
+            browser.console.warn(error);
         }
-        return $info_popup;
-    },
+    }
+}
 
-    _onClickInfo: function () {
-        var self = this;
-        var def = $.Deferred();
-        if (self.popover_initialized) {
-            def.resolve();
-        } else {
-            self._getInfoData().then(function (data) {
-                var $info_popup = self._renderInfoPopUp(data);
-                self.$info_icon.popover({
-                    content: $info_popup,
-                    html: true,
-                    trigger: 'manual',
-                    animation: true,
-                    template: qweb.render(
-                        'crnd_web_m2o_info_widget.popover_template', {}),
-                });
+M2OInfoPopover.template = "crnd_web_m2o_info_widget.popover_template";
+M2OInfoPopover.props = {
+    close: { type: Function, optional: true },
+    info: { type: Array, optional: true },
+};
 
-                self.popover_initialized = true;
+export class M2OInfoField extends Many2OneField {
+    setup() {
+        super.setup();
+        this.orm = useService("orm");
+        this.popover = usePopover();
+        this.infoIconRef = useRef("infoIcon");
+        this.state = useState({
+            isOpen: false,
+            loading: false,
+            info: [],
+        });
+        this._closePopover = null;
+    }
 
-                // Keep popover info, to be able to destroy when widget
-                // desttroyed
-                self.popover_data = self.$info_icon.data('bs.popover');
-                self.popover_field_data = data;
+    get hasInfoButton() {
+        return Boolean(this.props.readonly && this.props.value);
+    }
 
-                $info_popup.on('mouseleave', function () {
-                    self.$info_icon.popover('hide');
-                });
+    get infoFields() {
+        const fields = this.props.info_fields;
+        return Array.isArray(fields) ? fields : null;
+    }
 
-                def.resolve();
-            });
+    get infoMethod() {
+        const method = this.props.info_method;
+        return typeof method === "string" ? method : null;
+    }
+
+    async _getInfoDataFields() {
+        const fields = this.infoFields || [];
+        const [recordData] = await this.orm.read(this.relation, [this.resId], fields, {
+            context: this.context,
+        });
+        const fieldsGet = await this.orm.call(this.relation, "fields_get", [fields, ["string"]], {
+            context: this.context,
+        });
+        return fields.map((name) => ({
+            name,
+            string: fieldsGet?.[name]?.string || "",
+            value: recordData?.[name] ?? "",
+        }));
+    }
+
+    async _getInfoDataMethod() {
+        return this.orm.call(this.relation, this.infoMethod, [[this.resId]], {
+            context: this.context,
+        });
+    }
+
+    async _getInfoData() {
+        if (this.infoFields) {
+            return this._getInfoDataFields();
+        } else if (this.infoMethod) {
+            return this._getInfoDataMethod();
+        }
+        browser.console.warn("Cannot many2one field info. Field is not configured.");
+        return [];
+    }
+
+    async onClickInfo(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        if (this.state.isOpen) {
+            this._closePopover?.();
+            this._closePopover = null;
+            this.state.isOpen = false;
+            return;
         }
 
-        def.then(function () {
-            self.$info_icon.popover('toggle');
-        });
-    },
+        if (!this.resId) {
+            return;
+        }
 
-    _destroyPopover: function () {
-        if (this.popover_initialized) {
-            if (!this.$info_icon.data('bs.popover')) {
-                this.$info_icon.data('bs.popover', this.popover_data);
+        this.state.loading = true;
+        try {
+            this.state.info = await this._getInfoData();
+        } finally {
+            this.state.loading = false;
+        }
+
+        this._closePopover = this.popover.add(
+            this.infoIconRef.el,
+            M2OInfoPopover,
+            {
+                info: this.state.info,
+            },
+            {
+                popoverClass: "m2o-info-popover",
+                position: "bottom",
+                onClose: () => {
+                    this.state.isOpen = false;
+                    this._closePopover = null;
+                },
             }
-            if (this.popover_field_data) {
-                _.each(this.popover_field_data, function (field_info) {
-                    if (field_info.clipboard) {
-                        field_info.clipboard.destroy();
-                    }
-                });
-            }
-            this.$info_icon.popover('dispose');
-            this.popover_initialized = false;
-            this.popover_data = null;
-            this.popover_field_data = null;
-        }
-    },
+        );
+        this.state.isOpen = true;
+    }
+}
 
-    reset: function () {
-        this._destroyPopover();
-        this._super.apply(this, arguments);
-    },
+M2OInfoField.template = "crnd_web_m2o_info_widget.M2OInfoField";
+M2OInfoField.components = Many2OneField.components;
+M2OInfoField.supportedTypes = ["many2one"];
+M2OInfoField.props = {
+    ...Many2OneField.props,
+    info_fields: { type: Array, optional: true },
+    info_method: { type: String, optional: true },
+};
 
-    destroy: function () {
-        this._destroyPopover();
-        this._super.apply(this, arguments);
-    },
-});
+M2OInfoField.extractProps = ({ attrs, field }) => {
+    const res = Many2OneField.extractProps({ attrs, field });
+    return {
+        ...res,
+        info_fields: attrs.options?.info_fields,
+        info_method: attrs.options?.info_method,
+    };
+};
 
-fieldRegistry.add('m2o_info', M2OInfo);
+registry.category("fields").add("m2o_info", M2OInfoField);
