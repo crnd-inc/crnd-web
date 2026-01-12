e5e31d3a97df8073513be3a6bbccd82b3039bde5
Oleksandr Nechyporenko <ANechipor@gmail.com>
2026-01-12 16:49:11 +0200

    Handle error when filed not exists in model

--- crnd_web_m2o_info_widget/__manifest__.py
+++ crnd_web_m2o_info_widget/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'license': 'LGPL-3',
-    'version': '19.0.0.11.1',
+    'version': '19.0.0.11.2',
 
     'depends': [
         'web',
--- crnd_web_m2o_info_widget/tools/utils.py
+++ crnd_web_m2o_info_widget/tools/utils.py
@@ -14,9 +14,12 @@ def helper_get_many2one_info_data(record, field_names):
     record.ensure_one()
     res = []
     for field_name in field_names:
+        field = record._fields.get(field_name)
+        if not field:
+            continue
         res += [{
             'value': record[field_name],
-            'string': record._fields[field_name].get_description(
+            'string': field.get_description(
                 record.env)['string'],
             'name': field_name,
         }]
